fcfab3d8ec05641c98a8374c662dcb19f6f1477f
2026-DEV-052 <gsaintjean.externe@bedrockstreaming.com>
2026-06-21 15:29:12 +0200

    red phase : five-hour tests creation

--- app/src/main/java/com/example/berlinclock/domain/model/BerlinClock.kt
+++ app/src/main/java/com/example/berlinclock/domain/model/BerlinClock.kt
@@ -1,5 +1,6 @@
 package com.example.berlinclock.domain.model
 
 data class BerlinClock(
-    val second: Boolean,
+    val second: Boolean = false,
+    val hoursBy5: List<Boolean> = listOf(false, false, false, false),
 )
--- app/src/test/java/com/example/berlinclock/ConvertTimeToBerlinClockUseCaseTest.kt
+++ app/src/test/java/com/example/berlinclock/ConvertTimeToBerlinClockUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.example.berlinclock
 
 import com.example.berlinclock.domain.usecase.ConvertTimeToBerlinClockUseCase
 import org.junit.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -10,12 +11,62 @@ class ConvertTimeToBerlinClockUseCaseTest {
     val useCase = ConvertTimeToBerlinClockUseCase()
 
     @Test
-    fun `seconds led is lit during even seconds `() {
+    fun `seconds led is lit during even seconds`() {
         assertTrue { useCase(hours = 0, minutes = 0, seconds = 2).second }
     }
 
     @Test
-    fun `seconds led is off during odd seconds `() {
+    fun `seconds led is off during odd seconds`() {
         assertFalse { useCase(hours = 0, minutes = 0, seconds = 21).second }
+    }
+
+    @Test
+    fun `1 five-hour led lit when the number of hours is less than 10`() {
+        val expected = listOf(true, false, false, false)
+
+        assertEquals(
+            expected = expected,
+            actual = useCase(hours = 7, minutes = 0, seconds = 0).hoursBy5
+        )
+    }
+
+    @Test
+    fun `2 five-hour led lit when the number of hours is less than 15`() {
+        val expected = listOf(true, true, false, false)
+
+        assertEquals(
+            expected = expected,
+            actual = useCase(hours = 12, minutes = 0, seconds = 0).hoursBy5
+        )
+    }
+
+    @Test
+    fun `3 five-hour led lit when the number of hours is less than 20`() {
+        val expected = listOf(true, true, true, false)
+
+        assertEquals(
+            expected = expected,
+            actual = useCase(hours = 19, minutes = 0, seconds = 0).hoursBy5
+        )
+    }
+
+    @Test
+    fun `4 five-hour led lit when the number of hours is equal to 20`() {
+        val expected = listOf(true, true, true, true)
+
+        assertEquals(
+            expected = expected,
+            actual = useCase(hours = 20, minutes = 0, seconds = 0).hoursBy5
+        )
+    }
+
+    @Test
+    fun `all five-hour led are off when the number of hours is less than 5`() {
+        val expected = listOf(false, false, false, false)
+
+        assertEquals(
+            expected = expected,
+            actual = useCase(hours = 3, minutes = 0, seconds = 0).hoursBy5
+        )
     }
 }
