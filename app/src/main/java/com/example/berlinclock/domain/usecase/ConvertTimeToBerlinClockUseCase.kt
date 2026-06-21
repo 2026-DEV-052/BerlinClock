1ad9c6ef87630d6a59536a86f982c8c701ceeb47
2026-DEV-052 <gsaintjean.externe@bedrockstreaming.com>
2026-06-21 17:48:29 +0200

    green phase : solved five-hour tests

--- app/src/main/java/com/example/berlinclock/domain/usecase/ConvertTimeToBerlinClockUseCase.kt
+++ app/src/main/java/com/example/berlinclock/domain/usecase/ConvertTimeToBerlinClockUseCase.kt
@@ -5,7 +5,14 @@ import com.example.berlinclock.domain.model.BerlinClock
 class ConvertTimeToBerlinClockUseCase {
     operator fun invoke(hours: Int, minutes: Int, seconds: Int): BerlinClock {
         return BerlinClock(
-            second = seconds % 2 == 0
+            second = seconds % 2 == 0,
+            hoursBy5 = (hours / 5).toLit(4)
         )
+    }
+
+    private fun Int.toLit(size: Int): List<Boolean> {
+        return (1..size).map {
+            it <= this
+        }
     }
 }
